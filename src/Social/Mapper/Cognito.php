6c6c086bb24f6f9e4cdb452e827561e60106234d
Marcelo Rocha <contato@omarcelo.com.br>
2019-03-29 13:35:56 -0300

    Cognito mapper from users plugin (c6b52f15a40e836f75989396d72c02be640a8367)

---
+++ src/Social/Mapper/Cognito.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Copyright 2010 - 2017, Cake Development Corporation (https://www.cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2010 - 2017, Cake Development Corporation (https://www.cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+namespace CakeDC\Auth\Social\Mapper;
+
+use Cake\Utility\Hash;
+
+/**
+ * Cognito Mapper
+ *
+ */
+class Cognito extends AbstractMapper
+{
+
+    /**
+     * Map for provider fields
+     * @var
+     */
+    protected $_mapFields = [
+        'id' => 'sub',
+        'zoneinfo' => 'zoneinfo',
+        'link' => 'website',
+        'bio' => 'profile',
+        'first_name' => 'given_name',
+        'avatar' => 'picture',
+        'last_name' => 'family_name',
+    ];
+
+    /**
+     * Get link property value
+     *
+     * @param mixed $rawData raw data
+     *
+     * @return string
+     */
+    protected function _link($rawData)
+    {
+        return Hash::get($rawData, $this->_mapFields['link'], '#');
+    }
+
+    /**
+     *  Get first_name property value
+     *
+     * @param mixed $rawData raw data
+     *
+     * @return mixed
+     */
+    protected function _firstName($rawData)
+    {
+        return $this->getNameValue($rawData, 'first_name', 0);
+    }
+
+    /**
+     * Get last_name property value
+     *
+     * @param mixed $rawData raw data
+     *
+     * @return mixed
+     */
+    protected function _lastName($rawData)
+    {
+        return $this->getNameValue($rawData, 'last_name', 1);
+    }
+
+    /**
+     * Helper function to get a name portion(fist_name, last_name) value
+     *
+     * @param mixed $rawData raw data
+     * @param string $field Name field (first_name, last_name)
+     * @param int $keyInName key of string part in name field after exploder(' ', $value)
+     *
+     * @return string|null
+     */
+    private function getNameValue($rawData, $field, $keyInName)
+    {
+        $value = Hash::get($rawData, $this->_mapFields[$field]);
+        if ($value === null) {
+            $names = explode(' ', Hash::get($rawData, 'name', ''));
+
+            return isset($names[$keyInName]) ? $names[$keyInName] : null;
+        }
+
+        return (string)$value;
+    }
+}
