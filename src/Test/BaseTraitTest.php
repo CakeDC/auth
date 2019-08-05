6a56905a9439f2d6c27e0f061911a896fd5c8615
Marcelo Rocha <contato@omarcelo.com.br>
2019-08-05 11:44:32 -0300

    Renamed BaseTraitTest to BaseTestTrait

---
+++ src/Test/BaseTraitTest.php
@@ -0,0 +1,4 @@
+<?php
+// @deprecated 4.0.2 Load new trait and alias
+trait_exists('CakeDC\Auth\Test\BaseTestTrait');
+deprecationWarning('Use CakeDC\Auth\Test\BaseTestTrait instead of CakeDC\Auth\Test\BaseTraitTest.');
